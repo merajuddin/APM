7379e96cd6b82f76bc4e1ba24abdc5184984f366
mohammed merajuddin <meraj6054@gmail.com>
2017-11-08 18:02:07 -0600

    Created a Product Service
    registered the product service as a provider in the root component and
    injected inside the productList component to fetch the product data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ProductService } from "./products/products.service";
 
 @Component({
   selector:'pm-root',
@@ -7,7 +8,8 @@ import { Component } from "@angular/core";
               <h1> {{pageTitle}} </h1>
                 <pm-products></pm-products>
             </div>
-          `
+          `,
+  providers: [ ProductService ]
 })
 export class AppComponent{
   pageTitle: string = "Acme Product Management";
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Iproduct } from './product';
+import { ProductService } from './products.service';
 
 @Component({
     selector: 'pm-products',
@@ -23,31 +24,14 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: Iproduct[];
-    products: Iproduct[] = [
-    {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    products: Iproduct[]
+
+    constructor(private _productService: ProductService){
     }
-];
-    constructor(){
+
+    ngOnInit(): void{
+        this.products = this._productService.getProducts();
         this.filteredProducts = this.products;
-        this.listFilter = "cart";
     }
 
     performFilter(filterBy: string): Iproduct[]{
@@ -61,9 +45,6 @@ export class ProductListComponent implements OnInit{
         this.showImage = !this.showImage;
     }
 
-    ngOnInit(): void{
-        console.log("On Init");
-    }
     
     onRatingClicked(message: string): void{
         this.pageTitle = "Product List: "+ message;
